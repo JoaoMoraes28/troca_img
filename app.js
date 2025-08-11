add7ea07523dfb14c08158aad7fc2b81f0f2c261
JOÃO VICTOR SANTOS DE MORAES <25132878@dsn127m1247443.cfp127aluno.local>
2025-08-11 11:20:51 -0300

    funcao de carrossel ainda a fazer

--- app.js
+++ app.js
@@ -3,11 +3,40 @@
 const botaoImagem = document.getElementById('imagem')
 const botaoAuto = document.getElementById('auto')
 const img = document.getElementById('imgBg')
+const carrossel = document.getElementById('imgBgCarrossel')
 
 function trocarImg() {
-    const imgValue = `./img/${document.getElementById('texto').value}.jpg`
+    let textValue = document.getElementById('texto').value
+    let imgValue = `./img/${document.getElementById('texto').value}.jpg`
     console.log(imgValue)
     img.src = `${imgValue}`
+    
+    if(textValue == 'leao') {
+
+        document.documentElement.style.backgroundColor = '#E69953'
+
+    } else if(textValue == 'aguia'){
+
+        document.documentElement.style.backgroundColor = '#D3E0EB'
+
+    } else if(textValue == 'cobra'){
+
+        document.documentElement.style.backgroundColor = '#78320B'
+
+    } else if(textValue == 'macaco'){
+
+        document.documentElement.style.backgroundColor = '#719444'
+
+    } else if(textValue == 'crocodilo'){
+
+        document.documentElement.style.backgroundColor == '#489C70'
+
+    }
+}
+
+function autoImagem() {
+    carrossel.style.opacity = '1'
 }
 
 botaoImagem.addEventListener('click' , trocarImg)
+botaoAuto.addEventListener('click', autoImagem)
