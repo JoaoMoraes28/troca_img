b46be627e174b8add6fd0d92003d30040cd849ce
JOÃO VICTOR SANTOS DE MORAES <25132878@CFP127ALUNO.LOCAL>
2025-08-19 09:49:44 -0300

    projeto meia bomba, mas feito

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# troca_img
+# Troca de imagens
+
+Troca automática e manual de imagens do reino animal
+
+![](./img/Captura%20de%20tela%202025-08-19%20094839.png)
+Layout para Desktop.
+
+## Tecnologias
+* HTML
+* CSS
+* JS
+
+## Autor
+João Victor Santos de Moraes
--- app.js
+++ app.js
@@ -3,39 +3,17 @@
 const botaoImagem = document.getElementById('imagem')
 const botaoAuto = document.getElementById('auto')
 const img = document.getElementById('imgBg')
-const carrossel = document.getElementById('imgBgCarrossel')
+const carrossel = document.getElementById('imgBg')
 
 function trocarImg() {
     let textValue = document.getElementById('texto').value
     let imgValue = `./img/${document.getElementById('texto').value}.jpg`
     console.log(imgValue)
     img.src = `${imgValue}`
-    
-    if(textValue == 'leao') {
-
-        document.documentElement.style.backgroundColor = '#E69953'
-
-    } else if(textValue == 'aguia'){
-
-        document.documentElement.style.backgroundColor = '#D3E0EB'
-
-    } else if(textValue == 'cobra'){
-
-        document.documentElement.style.backgroundColor = '#78320B'
-
-    } else if(textValue == 'macaco'){
-
-        document.documentElement.style.backgroundColor = '#719444'
-
-    } else if(textValue == 'crocodilo'){
-
-        document.documentElement.style.backgroundColor == '#489C70'
-
-    }
 }
 
 function autoImagem() {
-    carrossel.style.opacity = '1'
+    carrossel.classList.add('imgBgCarrosel')
 }
 
 botaoImagem.addEventListener('click' , trocarImg)
